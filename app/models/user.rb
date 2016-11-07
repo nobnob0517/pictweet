f2ccfbd33d2d861d7dd0a896a304b69339be693b
nobmatsumura <akinobu.matsumura@gmail.com>
2016-11-07 16:57:40 +0900

    pic tweetコミット

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :tweets
+  has_many :comments
+
+  validates :nickname, presence: true, length: { maximum: 6 }
+
+end
